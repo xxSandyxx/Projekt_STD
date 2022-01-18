9f9d5fe9b5e3608e06d9185c053465847cdffbe3
Aleksandra <31063650+xxSandyxx@users.noreply.github.com>
2022-01-18 14:26:57 +0100

    Add files via upload

---
+++ dp1/dp1/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h: dołącz plik do standardowych systemowych plików dołączanych,
+// lub specyficzne dla projektu pliki dołączane, które są często wykorzystywane, ale
+// są rzadko zmieniane
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Wyklucz rzadko używane rzeczy z nagłówków systemu Windows
+// Pliki nagłówkowe systemu Windows:
+#include <windows.h>
+
+// Pliki nagłówkowe środowiska uruchomieniowego C
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+// TODO: W tym miejscu odwołaj się do dodatkowych nagłówków wymaganych przez program
